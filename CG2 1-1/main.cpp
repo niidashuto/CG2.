7e251f41df77a8635a7351b064ebfaa33b2b8913
niida_shuto <k021g1267@g.neec.ac.jp>
2022-04-13 15:04:08 +0900

    ウィンドウ表示

--- CG2 1-1/main.cpp
+++ CG2 1-1/main.cpp
@@ -1,4 +1,12 @@
 #include<Windows.h>
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#include<cassert>
+#include<vector>
+#include<string>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
 
 //ウィンドウプロシージャ
 LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
@@ -52,6 +60,47 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	MSG msg{};//メッセージ
 	//DirectX初期化　ここから
+	HRESULT result;
+	ID3D12Device* devise = nullptr;
+	IDXGIFactory7* dxgifactory = nullptr;
+	IDXGISwapChain4* swapChain = nullptr;
+	ID3D12CommandAllocator* cmdAllocator = nullptr;
+	ID3D12GraphicsCommandList* commandList = nullptr;
+	ID3D12CommandQueue* CommandQueue = nullptr;
+	ID3D12DescriptorHeap* rtvHeap = nullptr;
+
+	//DXGIファクトリーの生成
+	result = CreateDXGIFactory(IID_PPV_ARGS(&dxgifactory));
+	assert(SUCCEEDED(result));
+
+	//アダプターの列挙用
+	std::vector<IDXGIAdapter4*>adapters;
+	//ここに特定の名前を持つアダプターオブジェクトが入る
+	IDXGIAdapter4* tmpAdapter = nullptr;
+
+	//パフォーマンスが高いものから順に、全てのアダプターを列挙する
+	for (UINT i = 0;
+		dxgifactory->EnumAdapterByGpuPreference(i,
+			DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
+			IID_PPV_ARGS(&tmpAdapter)) != DXGI_ERROR_NOT_FOUND;
+		i++) {
+		//動的配列に追加する
+		adapters.push_back(tmpAdapter);
+	}
+	//妥当なアダプターを選別する
+	for (size_t i = 0; i = adapters.size(); i++) {
+		DXGI_ADAPTER_DESC3 adapterDesc;
+		//アダプターの情報を取得する
+		adapters[i]->GetDesc3(&adapterDesc);
+
+		//ソフトウェアデバイスを回避
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+			//デバイスを採用してループを抜ける
+			tmpAdapter = adapters[i];
+			break;
+		}
+	}
+	//対応レベルの配列
 	
 	//ゲームループ
 	while (true) {
