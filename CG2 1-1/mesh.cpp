2e346bec95f7654c1516f8077bf4a0bc1a8d4117
niida_shuto <k021g1267@g.neec.ac.jp>
2022-05-25 13:39:12 +0900

    a

---
+++ CG2 1-1/mesh.cpp
@@ -0,0 +1,31 @@
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#include<cassert>
+#include<d3dcompiler.h>
+#include<DirectXMath.h>
+#include<math.h>
+using namespace DirectX;
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#include<Windows.h>
+#include<vector>
+#include<string>
+#define DIRECTINPUT_VERSION 0x0800
+#include<dinput.h>
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+#include<DirectXTex.h>
+#include"object.h"
+
+void Object::Initialize() {
+
+}
+
+void Object::Updata() {
+
+}
+
+void Object::Draw() {
+	
+}
---
+++ CG2 1-1/mesh.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#include<cassert>
+#include<d3dcompiler.h>
+#include<DirectXMath.h>
+#include<math.h>
+using namespace DirectX;
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#include<Windows.h>
+#include<vector>
+#include<string>
+#define DIRECTINPUT_VERSION 0x0800
+#include<dinput.h>
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+#include<DirectXTex.h>
+class Object {
+public:
+	void Initialize();
+
+	void Updata();
+
+	void Draw();
+};
